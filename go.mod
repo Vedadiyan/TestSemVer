10172285a303b201f80824265c97820d26bf2b4b
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-07-21 11:01:01 +0800

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module test
+module github.com/vedadiyan/TestSemVer
 
 go 1.20
--- pkg.go
+++ pkg.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func Test() {
-	fmt.Println("this is version 2")
+	fmt.Println("this is version 1")
 }
